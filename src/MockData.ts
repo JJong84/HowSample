18be9a838a8aa21e2b619efffd685c63bfa56dcc
Jongwon Jang <jangjwhw@gmail.com>
2024-12-15 19:42:07 +0900

    FIX MockData

--- src/MockData.ts
+++ src/MockData.ts
@@ -6,25 +6,13 @@ const KanYe: BreakResponse = [
         pitch: 4,
         target: {
             start: 0.0,
-            end: 34.58,
+            end: 34.58
         },
         original: {
-            start: 193.6099133268447,
-            end: 237.60985289002036,
-        },
-    },
-    {
-        speed: 1.27241005081943,
-        pitch: 4,
-        target: {
-            start: 178.5,
-            end: 217.3,
-        },
-        original: {
-            start: 202.7712656985844,
-            end: 252.14077567037825,
-        },
-    },
+            start: 193.41833050417105,
+            end: 237.42606032712425
+        }
+    }
 ];
 
 const OneMoreTime: MockUpSample[] = [
